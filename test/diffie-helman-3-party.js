c9ac155f86eab49483a206a3c0d355f0dcac07d2
Anakorn Kyavatanakij <anakornk@gmail.com>
2017-12-15 22:09:08 +0800

    encryption example down

--- test/diffie-helman-3-party.js
+++ test/diffie-helman-3-party.js
@@ -1,5 +1,7 @@
 var crypto = require('crypto');
-var eccrypto = require('eccrypto');
+var BigInteger = require('bigi')
+var ecurve = require('ecurve')
+var eccrypto = require('eccrypto'); // optional
 
 // var privateKey = crypto.randomBytes(32);
 // var publicKey = eccrypto.getPublic(privateKey);
@@ -17,8 +19,6 @@ var publicKeyC = eccrypto.getPublic(privateKeyC);
 //--------------
 
 
-var BigInteger = require('bigi') //npm install --save bigi@1.1.0
-var ecurve = require('ecurve') //npm install --save ecurve@1.0.0
 
 
 var privateKey = new Buffer("1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd", 'hex')
@@ -36,3 +36,28 @@ console.log(publicKeyA2.getEncoded(false))
 
 
 
+// // Key Derive
+
+// var pbkdf2 = require('pbkdf2')
+// var derivedKey = pbkdf2.pbkdf2Sync(publicKey, 'salt', 1, 256, 'sha512');
+
+// //Encryption
+// var CryptoJS = require("crypto-js");
+// var derivedKey2 = CryptoJS.PBKDF2(publicKey, 'salt', { keySize: 128 / 32, iterations: 1000 });
+// // var ciphertext = CryptoJS.AES.encrypt('my message', derivedKey);
+// // console.log(ciphertext)
+
+
+var derivedKey = crypto.pbkdf2Sync(publicKey, 'salt', 1, 256, 'sha512');
+let cipher = crypto.createCipher('aes-256-cbc',derivedKey);
+let encrypted = cipher.update('some clear text data', 'utf8', 'hex');
+console.log(encrypted);
+encrypted += cipher.final('hex'); //append block leftovers
+console.log(encrypted);
+
+console.log('----');
+const decipher = crypto.createDecipher('aes-256-cbc', derivedKey);
+let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+console.log(decrypted);
+decrypted += decipher.final('utf8');
+console.log(decrypted);
