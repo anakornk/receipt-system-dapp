4f3cb2d0973b752a453298ff64a062cefbe655f9
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-01-03 17:43:09 +0800

    remove test folder

--- test/Government.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.19;
-
-contract Government {
-  function Government() public {
-
-  }
-}
--- test/include.js
+++
@@ -1,6 +0,0 @@
-var crypto = require('crypto');
-
-var privateKeyA = crypto.randomBytes(32);
-console.log(privateKeyA.toString('hex'));
-
-modules.export
